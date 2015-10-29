b309e049b696bbb3014dd02458de6857cb411f0f
davidfabian <fabiand.01@gmail.com>
2015-10-29 13:21:20 +0300

    exercise 19 working, pep8 formatted

---
+++ excercises/task_19.py
@@ -0,0 +1,24 @@
+__author__ = 'd'
+
+'''
+"99 Bottles of Beer" is a traditional song in the United States and Canada.
+It is popular to sing on long trips, as it has a very repetitive format which is easy to memorize,
+and can take a long time to sing. The song's simple lyrics are as follows:
+
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down, pass it around, 98 bottles of beer on the wall.
+
+The same verse is repeated, each time with one fewer bottle.
+The song is completed when the singer or singers reach zero.
+
+Your task here is write a Python program capable of generating all the verses of the song.
+'''
+
+
+def bottle():
+    for n in range(99, 0, -1):
+        print(n, "bottles of beer on the wall,", n, "bottles of beer.")
+        print("Take one down, pass it around,", n-1, "bottles of beer on the wall.")
+        print()
+
+print(bottle())
