3df33ba8fb4c3423fa6eda26724a65c7ad767c47
davidfabian <fabiand.01@gmail.com>
2015-11-02 12:26:59 +0300

    exercise 23 working, pep8 formatted

---
+++ excercises/task_23.py
@@ -0,0 +1,19 @@
+__author__ = 'd'
+'''
+Define a simple "spelling correction" function correct() that takes a string and sees to it that 1)
+two or more occurrences of the space character is compressed into one, and 2) inserts an extra space after a period if
+the period is directly followed by a letter. E.g. correct("This   is  very funny  and    cool.Indeed!") should return
+"This is very funny and cool. Indeed!" Tip: Use regular expressions!
+'''
+
+import re
+
+
+def correct(a):
+    result = (re.sub(' +', ' ', a))
+    # " +" means more than one whitespaces
+    result = re.sub('.(?=[A-Z])', '. ', result)
+    # "[.]" means any "." will be replaced
+    print(result)
+
+correct("This   is  very funny  and    cool.Indeed!")
